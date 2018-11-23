1091d6918a8adcc7d564288dff4ebfd0c0a79d85
sunshixiong <1285913468@qq.com>
2018-11-23 10:31:03 +0800

    添加代理服务器

--- src/views/bannerManage/BannerManage.tsx
+++ src/views/bannerManage/BannerManage.tsx
@@ -1,23 +1,34 @@
-import {createStyles, Theme, withStyles} from '@material-ui/core/styles';
-import {WithStyles} from "@material-ui/core/styles/withStyles";
+import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
+import { WithStyles } from "@material-ui/core/styles/withStyles";
 import * as React from "react";
+import { Button } from "antd";
+import service from "../../axios/Service";
 
-const styles = (theme: Theme) => createStyles<"BannerManage">({
+const styles = (theme: Theme) =>
+  createStyles<"BannerManage">({
     BannerManage: {}
-});
+  });
 
-interface Iprops extends WithStyles<typeof styles> {
-}
+interface Iprops extends WithStyles<typeof styles> {}
 
 class BannerManage extends React.Component<Iprops> {
-    public render() {
-        const {classes} = this.props;
-        return (
-            <div className={classes.BannerManage}>
-                图片管理
-            </div>
-        );
-    }
+  public render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.BannerManage}>
+        图片管理
+        <Button
+          onClick={() => {
+            service.get("/user/getAccountInfo").then(value => {
+              alert(JSON.stringify(value.data));
+            });
+          }}
+        >
+          请求
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(BannerManage);
