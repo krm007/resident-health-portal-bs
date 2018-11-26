0820600b553ec379df90f16cefa180e436b30af5
sunshixiong <1285913468@qq.com>
2018-11-26 09:31:34 +0800

    修改配置文件和数据请求文件

--- src/axios/Service.ts
+++ src/axios/Service.ts
@@ -1,30 +1,46 @@
+import { Modal } from "antd";
 import Axios from "axios";
+import createHashHistory from "history/createHashHistory";
 
 const service = Axios.create({
-    timeout: 15000
+  timeout: 15000
 });
 
 /**
  * 添加请求拦截器(在发送请求之前或之后做什么)
  */
 service.interceptors.request.use(
-    config => {
-        return config;
-    },
-    error => {
-        return Promise.reject(error);
-    }
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
 );
 
 /**
  * 添加响应拦截器
  */
 service.interceptors.response.use(
-    response => {
-        return response;
-    },
-    error => {
-        return Promise.reject(error);
+  response => {
+    if (response.status === 401) {
+      Modal.confirm({ content: "登录信息已过期！请重新登录" });
+      createHashHistory().push("/login");
     }
+    return response;
+  },
+  error => {
+    if (error.response.status === 500) {
+      Modal.error({ content: "服务器错误" });
+    }
+    if (error.response.status === 504 || error.response.status === 404) {
+      Modal.error({ content: "服务器被吃了⊙﹏⊙∥" });
+    }
+    if (error.response.status === 401) {
+      const historyApp = createHashHistory();
+      historyApp.push("/login");
+    }
+    return Promise.reject(error);
+  }
 );
 export default service;
