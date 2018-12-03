c40bf1c3cb8cc7526144a5b2e1467eff54b174e3
sunshixiong <1285913468@qq.com>
2018-12-03 10:09:01 +0800

    黑名单

--- src/axios/Request.ts
+++ src/axios/Request.ts
@@ -1,8 +1,7 @@
-
 import service from "./Service";
 import { RestPage } from "../type/CommonData";
 import { BlacklistData, Sources } from "../type/SourcesData";
-import {PushList, OneList, AddNews} from "../type/MessageData";
+import { PushList, OneList, AddNews } from "../type/MessageData";
 
 // 获取日志列表
 export function getLogList() {
@@ -30,32 +29,34 @@ export function getDocDic() {
 /**
  * 获取号源
  */
-export function getSources() {
-  return service.get<RestPage<Sources>>("/sources");
+export function getSources(params: any) {
+  return service.get<RestPage<Sources>>("/sources/query", { params });
 }
 /**
  * 获取黑名单列表
  */
-export function getBlacklist() {
-  return service.get<RestPage<BlacklistData>>("/blackLists")
+export function getBlacklist(params: any) {
+  return service.get<RestPage<BlacklistData>>("/blackLists/query", {
+    params
+  });
 }
 /*
-*
-* 获取推送列表*/
+ *
+ * 获取推送列表*/
 export function getInfoPushList() {
-    return service.get<RestPage<PushList>>("/news")
+  return service.get<RestPage<PushList>>("/news");
 }
 /*
-*
-* 获取一条推送列表*/
-export function getInfoOneList(id:any) {
-    return service.get<OneList>(`/news/${id}`)
+ *
+ * 获取一条推送列表*/
+export function getInfoOneList(id: any) {
+  return service.get<OneList>(`/news/${id}`);
 }
 // 新增一条推送
-export function addNew(data:AddNews) {
-    return service.post("/news",data)
+export function addNew(data: AddNews) {
+  return service.post("/news", data);
 }
 // 删除一条推送
-export function delateNew(id:string) {
-  return service.delete(`/news/${id}`)
+export function delateNew(id: string) {
+  return service.delete(`/news/${id}`);
 }
--- src/views/blacklist/Blacklist.tsx
+++ src/views/blacklist/Blacklist.tsx
@@ -1,11 +1,12 @@
 import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
 import { WithStyles } from "@material-ui/core/styles/withStyles";
 import * as React from "react";
-import { Form, Input, Table } from "antd";
+import { Divider, Form, Input, Modal, Radio, Select, Table } from "antd";
 import { FormComponentProps } from "antd/lib/form";
 import { BlacklistData } from "../../type/SourcesData";
 import { getBlacklist } from "../../axios/Request";
 import { ColumnProps } from "antd/lib/table";
+import RadioGroup from "antd/lib/radio/group";
 
 const styles = (theme: Theme) =>
   createStyles<"root" | "sss">({
@@ -20,63 +21,165 @@ const styles = (theme: Theme) =>
 interface Iprops extends WithStyles<typeof styles>, FormComponentProps {}
 interface Istates {
   data: BlacklistData[];
+  blackStates: boolean;
+  recordStates: boolean;
+  recordList:any[]
 }
-
+/**
+ * 用户黑名单管理
+ */
 class Blacklist extends React.Component<Iprops, Istates> {
+  private queryData: { username?: string; verifyStatus?: string } = {};
   private tableColumns: Array<ColumnProps<BlacklistData>> = [
     {
       title: "用户名",
+      align: "center",
       dataIndex: "username"
     },
     {
       title: "创建时间",
+      align: "center",
       dataIndex: "createTime"
     },
     {
       title: "账号状态",
+      align: "center",
       dataIndex: "status"
     },
     {
       title: "违约次数",
+      align: "center",
       dataIndex: "defaultTimes"
+    },
+    {
+      title: "操作",
+      align: "center",
+      render: (text, record, index) => {
+        if (record.verifyStatus !== "1") {
+          return (
+            <span>
+              <a
+                onClick={this.checkRecord.bind(this, record.id)}
+                style={{ marginRight: 10 }}
+              >
+                违约记录
+              </a>
+              <a onClick={this.addBlack.bind(this, record.id)}>加入黑名单</a>
+            </span>
+          );
+        } else {
+          return <a>违约记录</a>;
+        }
+      }
     }
   ];
   constructor(props: Iprops) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      blackStates: false,
+      recordStates: false,
+      recordList:[]
     };
   }
+  public checkRecord = (id: string) => {
+    this.setState({
+      blackStates: true
+    });
+  };
+  public addBlack = () => {
+    this.setState({
+      recordStates: false
+    });
+  };
   public UNSAFE_componentWillMount(): void {
-    getBlacklist().then(value => {
+    this.getData();
+  }
+  public getData = () => {
+    getBlacklist(this.queryData).then(value => {
       this.setState({
         data: value.data._embedded.blackLists
       });
     });
-  }
-
-  public searchData = () => {};
+  };
+  public searchData = (value: string) => {
+    this.queryData.username = value;
+    this.getData();
+  };
   public render() {
     const { getFieldDecorator } = this.props.form;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <Form
-          layout={"inline"}
-          onSubmit={this.searchData}
-          style={{ marginBottom: 20 }}
-        >
+        <Form layout={"inline"} style={{ marginBottom: 20 }}>
+          <Form.Item label={"黑名单"}>
+            {getFieldDecorator("verifyStatus", {
+              initialValue: "",
+              getValueFromEvent: args => {
+                this.queryData.verifyStatus = args;
+                this.getData();
+                return args;
+              }
+            })(
+              <Select style={{ width: "150px" }}>
+                <Select.Option value={""}>全部</Select.Option>
+                <Select.Option value={"1"}>黑名单</Select.Option>
+                <Select.Option value={"2"}>白名单</Select.Option>
+              </Select>
+            )}
+          </Form.Item>
           <Form.Item label={"用户名称"}>
             {getFieldDecorator("userName")(
               <Input.Search
                 placeholder="用户名称"
-                onSearch={value => console.log(value)}
+                onSearch={this.searchData}
                 enterButton={true}
               />
             )}
           </Form.Item>
         </Form>
-        <Table size={"middle"} dataSource={this.state.data} columns={this.tableColumns} />
+        <Table
+          size={"middle"}
+          dataSource={this.state.data}
+          columns={this.tableColumns}
+        />
+        <Modal
+          visible={this.state.blackStates}
+          title={"停用时间"}
+          centered={true}
+          onOk={() => {
+            this.setState({
+              blackStates: false
+            });
+          }}
+          onCancel={() => {
+            this.setState({
+              blackStates: false
+            });
+          }}
+        >
+          <div>
+            <RadioGroup>
+              <Radio value={1}>一个月</Radio>
+              <Radio value={3}>三个月</Radio>
+              <Radio value={6}>六个月</Radio>
+              <Radio value={12}>一年</Radio>
+            </RadioGroup>
+            <div style={{ color: "red" }}>
+              "进入黑名单用户将不能使用预约挂号功能，其它功能照常使用"
+            </div>
+          </div>
+        </Modal>
+        <Modal centered={true}>
+          {this.state.recordList.map((value, index) => {
+            return(
+                <div key={value.id}>
+                  <span>{}</span>
+                  <Divider/>
+                </div>
+            )
+          })}
+        </Modal>
       </div>
     );
   }
